6988c07d9d875763cacc6d1c0224af9d0c16160c
JanLuis27 <janelproxd@gmail.com>
2020-07-05 17:00:35 -0500

    Quite el formulario anterior

--- src/index.js
+++ src/index.js
@@ -6,62 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import { useFormik } from 'formik';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-//Aquí está el formulario pedorro del inicio
-class FlavorForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 'coco'};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('Webos: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        
-      <input class="input-text" placeholder="Nombre" type="text"/>
-      <input class="input-text" placeholder="Apellido"/>
-      <input class="input-text" placeholder="Apellido 2"/>
-      <input type="date"/>
-      <input placeholder="Que tanto te afecto el desastre?"/>
-      <input type="file" placeholder="Tu cv"/> 
-      o 
-      <textarea placeholder="describe tus capacidades"></textarea>
-      
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
-
-//validar
-
-//Aquí inicia el nuevo
 
 const SignupForm = () => {
   return (
     <Formik
       initialValues={{ firstName: '', lastName: '', email: '' , date: '', afect: '', cap: ''}}
       validationSchema={Yup.object({
-        firstName: Yup.string()
-          .max(40, 'Nombre debe ser de 40 caracteres o menos')
-          .required('Nombre requerido'),
-        lastName: Yup.string()
-          .max(20, 'Apellidos deben ser de 40 caracteres o menos')
-          .required('Apellidos Requeridos'),
-        email: Yup.string()
-          .email('Correo inválido')
-          .required('Correo Requerido'),
+        firstName: Yup.string().max(40, 'Nombre debe ser de 40 caracteres o menos').required('Nombre requerido'),
+        lastName: Yup.string().max(20, 'Apellidos deben ser de 40 caracteres o menos').required('Apellidos Requeridos'),
+        email: Yup.string().email('Correo inválido').required('Correo Requerido'),
         date: Yup.date().min("1920-01-01", "Favor de ingresar una fecha más reciente").max("2002-01-01", "Necesitas ser mayor de edad").required('Este campo es requerido'),
         afect: Yup.string().max(500, 'El límite es de 500 caracteres').required('Este campo es requerido'),
         cap: Yup.string().max(500, 'El límite es de 500 caracteres').required('Este campo es requerido')
