2e64f21771603ad6387748d710367d07db198005
JanLuis27 <janelproxd@gmail.com>
2020-07-06 15:01:41 -0500

    Formulario mas bonito

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ import * as serviceWorker from './serviceWorker';
 import { useFormik } from 'formik';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+	cuadros: {
+		padding: '0px 0px 30px 0px',
+	}
+});
 
 
 const SignupForm = () => {
+	const classes = useStyles();
   return (
     <Formik
       initialValues={{ firstName: '', lastName: '', email: '' , date: '', afect: '', cap: ''}}
@@ -26,28 +36,18 @@ const SignupForm = () => {
           setSubmitting(false);
         }, 400);
       }}
-    >
-      <Form>
-        <label htmlFor="firstName">Nombre:</label><br/>
-        <Field name="firstName" type="text" class="ing" />
-        <p class="error"><ErrorMessage name="firstName" /></p>
-        <label htmlFor="lastName">Apellido:</label><br/>
-        <Field name="lastName" type="text" class="ing" />
-        <p class="error"><ErrorMessage name="lastName" class="error" /></p>
-        <label htmlFor="email" >Correo:</label><br/>
-        <Field name="email" type="email" class="ing" />
-        <p class="error"><ErrorMessage name="email" class="error" /></p>
-       	<label htmlFor="date">Fecha:</label><br/>
-       	<Field name="date" type="date" />
-       	<p class="error"><ErrorMessage name="date" class="error" /></p>
-       	<label htmlFor="afect">¿Qué tanto te afectó el desastre?:</label><br/>
-       	<Field name="afect" as="textarea" />
-       	<p class="error"><ErrorMessage name="afect" class="error" /></p>
-       	<label htmlFor="cap">¿Cuáles son tus capacidades?:</label><br/>
-       	<Field name="cap" as="textarea" />
-       	<p class="error"><ErrorMessage name="cap" class="error" /></p>
-       	<button type="submit">Subir</button>
-      </Form>
+    >{formik=>(
+    	<Form onSubmit={formik.handleSubmit}>
+        {formik.touched.firstName && formik.errors.firstName ? (<TextField name="firstName" label={formik.errors.firstName} variant="filled" error/>) : (<TextField name="firstName" label="Nombre" variant="filled" className={classes.cuadros} />)}<br/>
+        {formik.touched.firstName && formik.errors.lastName ? (<TextField name="lastName" label={formik.errors.lastName} variant="filled" error/>) : (<TextField name="lastName" label="Apellidos" variant="filled" className={classes.cuadros} />)}<br/>
+        {formik.touched.email && formik.errors.email ? (<TextField name="email" label={formik.errors.email} variant="filled" error />) : (<TextField name="email" label="Correo electronico" variant="filled" className={classes.cuadros} />)}<br/>
+       	{formik.touched.date && formik.errors.date ? (<TextField type="date" error label={formik.errors.date} InputLabelProps={{shrink: true,}}/>) : (<TextField type="date" label="Fecha de nacimiento" InputLabelProps={{shrink: true,}} className={classes.cuadros} />)}<br/>
+       	{formik.touched.afect && formik.errors.afect ? (<TextField name="afect" label={formik.errors.afect} variant="filled" error/>) : (<TextField name="afect"  variant="filled" label="¿Qué tanto le afectó el desastre?" className={classes.cuadros} />)}<br/>
+       	{formik.touched.cap && formik.errors.cap ? (<TextField name="cap" label={formik.errors.cap} variant="filled" error/>) : (<TextField name="cap" variant="filled" label="¿Cuáles son sus capacidades?" className={classes.cuadros} />)}<br/>
+       	<Button type="submit" variant="contained" color="primary" >Subir</Button>
+      	</Form>
+    	)}
+      
     </Formik>
   );
 };
